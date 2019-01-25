2c5f4c550cdb11caaf14c73e25f496714ea71552
Michael55555 <michaelpeters5647@gmail.com>
2019-01-25 17:29:40 +0100

    Performance Improvements

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/', express.static('public'));
-app.get('/', (req, res) => {
-  res.send('It\'s working !');
-});
-
-app.listen(3333, () => console.log('Listening on port 88'));
