f13f5eda3f1ddfb8d4032c98dca5bb3f29988e79
Michael Peters <michaelpeters5647@gmail.com>
2021-06-07 21:36:29 +0200

    Make form submittable

---
+++ script.js
@@ -0,0 +1,51 @@
+onload = () => {
+  const submitButton = document.getElementById("submit-button");
+
+  submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+    const form = document.querySelector("form");
+
+    if (!validate(name, email, message)) return;
+
+    db.collection("messages")
+      .add({
+        name: name.value,
+        email: email.value,
+        message: message.value,
+      })
+      .then(() => (submitButton.textContent = "Thanks for reaching out!"))
+      .catch((err) => {
+        console.log(err);
+        submitButton.textContent = "Thanks for reaching out!";
+      });
+  });
+
+  function validate(name, email, message) {
+    const errBorder = "2px solid red";
+    let valid = true;
+    if (name.value) {
+      name.style.border = "none";
+    } else {
+      name.style.border = errBorder;
+      valid = false;
+    }
+    if (email.value) {
+      email.style.border = "none";
+    } else {
+      email.style.border = errBorder;
+      valid = false;
+    }
+    if (message.value) {
+      message.style.border = "none";
+    } else {
+      message.style.border = errBorder;
+      valid = false;
+    }
+
+    return valid;
+  }
+};
