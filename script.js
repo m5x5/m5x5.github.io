057f5ad2053f7afbf323ee7440166ef41d019119
Michael Peters <michaelpeters5647@gmail.com>
2021-06-20 08:11:47 +0200

    Remove border if focused

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 onload = () => {
   const submitButton = document.getElementById("submit-button");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+  const form = document.querySelector("form");
 
   submitButton.addEventListener("click", (e) => {
     e.preventDefault();
-
-    const name = document.getElementById("name");
-    const email = document.getElementById("email");
-    const message = document.getElementById("message");
-    const form = document.querySelector("form");
 
     if (!validate(name, email, message)) return;
 
@@ -22,6 +21,16 @@ onload = () => {
         console.log(err);
         submitButton.textContent = "Thanks for reaching out!";
       });
+  });
+
+  [name, email, message].forEach((input) => {
+    input.addEventListener("focus", (e) => {
+      input.style.border = "none";
+    });
+    input.addEventListener("focusout", (e) => {
+      console.log("Out of focus :,-(");
+      validate(name, email, message);
+    });
   });
 
   function validate(name, email, message) {
